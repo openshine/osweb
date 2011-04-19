832814546ec66ba0e84434f2b2e321b32511b95f
Pablo Vieytes <pvieytes@openshine.com>
2011-04-19 14:03:36 +0200

    add main views
    Signed-off-by: Pablo Vieytes <pvieytes@openshine.com>

--- urls.py
+++ urls.py
@@ -1,16 +1,20 @@
 from django.conf.urls.defaults import *
+from osweb import settings
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^osweb/', include('osweb.foo.urls')),
+                       
+    (r'^$', include('main.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
 )
+
+
+#If not in DJAPP engine add static media folder as static server
+if not 'DJAPPS_SERVER' in dir(settings):
+    urlpatterns += patterns('',
+        (r'^static_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    )
