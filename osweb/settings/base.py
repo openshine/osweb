cf56c2a228493b331c7877457230aab294b7ace1
César García Tapia <cesar.garcia.tapia@openshine.com>
2013-08-06 13:31:12 +0200

    Moved settings to osweb/settings and export sensible data to envvars

--- osweb/settings/base.py
+++ osweb/settings/base.py
@@ -18,26 +18,45 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
-# Django settings for osweb project.
 import os
+import os.path
+
+from django.core.exceptions import ImproperlyConfigured
+
+import dj_database_url
+
+def get_env_variable(var_name):
+    """ Get the environment variable or return exception """
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
 
 PROJECT_DIR = os.path.dirname(__file__)
+SYSCONF_DIR = os.path.join('@sysconfdir@', '@package@')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'osweb.db',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
-}
+DATABASES = {'default': dj_database_url.config()}
+
+TIME_ZONE = 'Europe/Madrid'
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+
+SECRET_KEY = get_env_variable("OSWEB_SECRET_KEY")
+
+ADMINS = (
+    ('Pablo Vieytes', 'pvieytes@openshine.com'),
+)
+MANAGERS = ADMINS
+
+USE_I18N = True
+USE_L10N = True
 
 MEDIA_ROOT = ''
 MEDIA_URL = ''
-
 STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
@@ -86,5 +105,45 @@ INSTALLED_APPS = (
     'osweb'
 )
 
-#Import all application definitions
-from defs import *
+#APP DEFINITIONS
+#-----------------------------------------------------
+NEWS_BLOG_URL = "http://blogs.openshine.com/feed/"
+
+MORE_BLOGS_URL = (
+    "http://blogs.openshine.com/planet/feed/", 
+)
+
+#BLOGS_CACHE_TIME : The timeout, in seconds, to use for the blogs cache
+BLOGS_CACHE_TIME = 60*60
+
+#NUM_HOME_NEWS : news number that will be displayed in home page
+NUM_HOME_NEWS = 2
+
+#NUM_HOME_PROJECTS : projects number that will be displayes in home page 
+NUM_HOME_PROJECTS = 3
+
+#NUM_LIVE_POSTS : post number that will be displayed in live page
+NUM_LIVE_POSTS = 8
+
+#NUM_LIVE_RECENT_POSTS : recent post number that will be displayed in live page
+NUM_LIVE_RECENT_POSTS = 8
+
+#TWEETS_CACHE_TIME : The timeout, in seconds, to use for the projects cache
+TWEETS_CACHE_TIME = 60*60
+NUM_TWEETS = 5
+TWITTER_USER = "openshine"
+
+#SLIDE_TIMER : Number of miliseconds per slide at home
+SLIDE_TIMER = 9000
+
+#PROJECTS_CACHE_TIME : The timeout, in seconds, to use for the projects cache
+PROJECTS_CACHE_TIME = 60*60     
+
+NUM_PROJECTS_HOME = 3
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
